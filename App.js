2f45756aa29965912c272c13af6c75ece8eaa3af
furkankokum <furkankokum50@gmail.com>
2022-10-16 22:27:10 +0300

    commit message

---
+++ App.js
@@ -0,0 +1,34 @@
+import OneSignal from 'react-native-onesignal';
+import React,{Component} from 'react';
+import {Container,Content,Text} from 'react-native'
+import{ImageBackground} from 'react-native';
+import {App_Id} from '@env'
+export default function App() {
+  return (
+    <ImageBackground style={{flex: 1}} source={{uri:"https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"}}>       
+    </ImageBackground> 
+  );
+}
+
+// OneSignal Initialization
+OneSignal.setAppId({App_Id});
+
+// promptForPushNotificationsWithUserResponse will show the native iOS or Android notification permission prompt.
+// We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 8)
+OneSignal.promptForPushNotificationsWithUserResponse();
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+  console.log("OneSignal: notification will show in foreground:", notificationReceivedEvent);
+  let notification = notificationReceivedEvent.getNotification();
+  console.log("notification: ", notification);
+  const data = notification.additionalData
+  console.log("additionalData: ", data);
+  // Complete with null means don't show a notification.
+  notificationReceivedEvent.complete(notification);
+});
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+  console.log("OneSignal: notification opened:", notification);
+});
